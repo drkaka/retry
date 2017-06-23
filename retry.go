cfc011fc53e8f5a0327318bf7dc4d6000b87e954
drkaka <drkaka@drkaka.com>
2017-06-23 09:30:42 +0800

    Pass in retry times.

--- retry.go
+++ retry.go
@@ -6,20 +6,21 @@ import (
 
 // Call to use retry functions
 //
+// f (times int) bool will passin current retry times and if success, return true
 // times to retry the function
 // interval the initial seconds used to wait for another call
 // factor to be mutiplied to interval after every retry
-func Call(f func() bool, times, interval int, factor float32) bool {
-	if f() {
+func Call(f func(int) bool, times, interval int, factor float32) bool {
+	if f(0) {
 		return true
 	}
 
 	wait := interval
-	for index := 0; index < times; index++ {
+	for i := 0; i < times; i++ {
 		time.Sleep(time.Duration(wait) * time.Second)
 		wait = int(float32(wait) * factor)
 
-		if f() {
+		if f(i + 1) {
 			return true
 		}
 	}
